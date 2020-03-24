a82e3b20fc5082d85e53b36c621d56d2ad893d0e
vince87 <vincenzo.bini@gmail.com>
2020-03-24 13:12:13 +0100

    Create jammapi_version.sh

---
+++ script/jammapi_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dialog --title "Versione JammaPi" \
+--backtitle "Menu versione JammaPi" \
+--yesno "Usi la nuova JammaPi Final?" 6 60
+
+response=$?
+case $response in
+   0) mv JammaPi/script/jammapi_new.sh JammaPi/script/jammapi.sh
+   1) mv JammaPi/script/jammapi_old.sh JammaPi/script/jammapi.sh
+esac
