d9c1634b810908f3634bb7bf50141daf7c837b99
vince87 <vincenzo.bini@gmail.com>
2020-03-24 12:37:54 +0100

    Update jammapi.sh

--- script/jammapi.sh
+++ script/jammapi.sh
@@ -18,10 +18,6 @@
 	modprobe joypi
   fi
   
-  CRT=/boot/JAMMA.txt
-  VGA=/boot/VGA.txt
-  HDMI=/boot/HDMI.txt
-  
   SCRIPT=/boot/autorun.jammapi
    
   ##Check if JammaPi script is installed
@@ -34,49 +30,65 @@
 	sleep 5
 	exit
 	fi
-  
-  ##Check if exist CRT
-  printf "\033[1;31m Controllo se esiste il file CRT \033[0m\n"
-  if test -f "$CRT"; then
-    echo "$FILE esite!"
+
+sudo grep '#CRT' /boot/config.txt > /dev/null 2>&1
+  if [ $? -eq 0 ] ; then
+    current=0
+  fi
+sudo grep '#VGA' /boot/config.txt > /dev/null 2>&1
+  if [ $? -eq 0 ] ; then
+    current=1
+  fi
+sudo grep '#HDMI' /boot/config.txt > /dev/null 2>&1
+  if [ $? -eq 0 ] ; then
+    current=2
+  fi
+
+plex1=$(gpio read 14)
+plex2=$(gpio read 12)
+
+echo $plex1
+echo $plex2
+
+if [ $plex1 = 0 ] && [ $plex2 = 0 ]; then
+  echo "JAMMA/SCART"
+  if [ $current != 0 ]; then
     bash /home/pi/JammaPi/script/interfaccia.sh -JAMMA
     printf "\033[0;32m !!!SWITCH COMPLETATO!!! \033[0m\n"
     printf "\033[0;32m     !!!RIAVVIO IN CORSO!!! \033[0m\n"
-    sudo rm /boot/JAMMA.txt
     sleep 5
-    sudo reboot
-  else
-    printf "\033[0;32m Skip! \033[0m\n"
+    #sudo reboot
   fi
-     
-  ##Check if exist VGA
-  printf "\033[1;31m Controllo se esiste il file VGA \033[0m\n"
-  if test -f "$VGA"; then
-    echo "$FILE esite!"
+fi
+
+if [ $plex1 = 0 ] && [ $plex2 = 1 ]; then
+  echo "VGA"
+  if [ $current != 1 ]; then
     bash /home/pi/JammaPi/script/interfaccia.sh -VGA
     printf "\033[0;32m !!!SWITCH COMPLETATO!!! \033[0m\n"
     printf "\033[0;32m     !!!RIAVVIO IN CORSO!!! \033[0m\n"
-    sudo rm /boot/VGA.txt
     sleep 5
-    sudo reboot
-  else
-  printf "\033[0;32m Skip! \033[0m\n"
+    #sudo reboot
   fi
-   
-  ##Check if exist HDMI
-  printf "\033[1;31m Controllo se esiste il file HDMI \033[0m\n"
-  if test -f "$HDMI"; then
-    echo "$FILE esite!"
+fi
+
+if [ $plex1 = 1 ] && [ $plex2 = 0 ]
+then
+  echo "HDMI"
+  if [ $current != 2 ]; then 
     bash /home/pi/JammaPi/script/interfaccia.sh -HDMI
     printf "\033[0;32m !!!SWITCH COMPLETATO!!! \033[0m\n"
     printf "\033[0;32m     !!!RIAVVIO IN CORSO!!! \033[0m\n"
-    sudo rm /boot/HDMI.txt
     sleep 5
-    sudo reboot
-  else
-    printf "\033[0;32m Skip! \033[0m\n"
+    #sudo reboot
   fi
-  
+fi
+
+if [ $plex1 = 1 ] && [ $plex2 = 1 ]
+then
+  echo "!ILLEGAL!"
+fi
+
     ##Check if exist autorun.jammapi
   printf "\033[1;31m Controllo se esiste il file autorun.jammapi \033[0m\n"
   if test -f "$SCRIPT"; then
